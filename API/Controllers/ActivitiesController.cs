2c6dbc499d418ab0c1d43324d93e70a7bbc5759e
Velin Arbov <Velin.Arbov@iris.co.uk>
2023-05-29 14:42:04 +0300

    Create Initial API with empty get empoints

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        public ActivitiesController() { }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            var activities = new List<Activity>();
+            return activities;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetById(Guid Id)
+        {
+            var activities = new List<Activity>();
+            return new Activity();
+        }
+    }
+}
