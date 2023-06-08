6757e361dbd0bf7cacb12927b551da24a5cf52f4
Velin Arbov <Velin.Arbov@iris.co.uk>
2023-06-08 15:38:32 +0300

    Implement .NET Identity

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using API.Dtos;
+using API.Services;
+using Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
+{
+    private readonly TokenService _tokenService;
+    private readonly UserManager<AppUser> _userManager;
+
+    public AccountController(UserManager<AppUser> userManager, TokenService tokenService)
+    {
+        _tokenService = tokenService;
+        _userManager = userManager;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _userManager.FindByEmailAsync(loginDto.Email);
+        if (user == null) return Unauthorized("ese");
+
+        var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+        if(!result) return Unauthorized("password");
+
+        return new UserDto
+        {
+            DisplayName = user.DisplayName,
+            Image = null,
+            Token = _tokenService.CreateToken(user),
+            Username = user.UserName
+        };
+    }
+}
---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using API.Services;
+using Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Persistence;
+
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<DataContext>();
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(("0ea9ed8a-1bda-422b-8c28-ec28192d41d5f146d808-c499-4928-88cc-1a734b349d92")));
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer((opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            }));
+        services.AddScoped<TokenService>();
+        return services;
+    }
+}
