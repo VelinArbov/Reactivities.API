be50bb65852ee9d5308968cd40044bde9365be4c
Velin Arbov <Velin.Arbov@iris.co.uk>
2023-06-12 18:11:41 +0300

    Fix error handling

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,5 +1,4 @@
 ﻿using Application.Core;
-using Microsoft.Extensions.Hosting;
 using System.Net;
 using System.Text.Json;
 
@@ -10,12 +9,12 @@ public class ExceptionMiddleware
     private readonly RequestDelegate _next;
     private readonly ILogger<ExceptionMiddleware> _logger;
     private readonly IHostEnvironment _env;
-
-    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
+        IHostEnvironment env)
     {
-        _next = next;
-        _logger = logger;
         _env = env;
+        _logger = logger;
+        _next = next;
     }
 
     public async Task InvokeAsync(HttpContext context)
@@ -36,6 +35,7 @@ public class ExceptionMiddleware
 
             var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
             var json = JsonSerializer.Serialize(response, options);
+
             await context.Response.WriteAsync(json);
         }
     }
