dc4056c3f4b9fb438ba988286cbedfb6a03a8a85
Velin Arbov <Velin.Arbov@iris.co.uk>
2023-06-15 15:25:29 +0300

    FIx

--- API/Mappers/MapperProfile.cs
+++ API/Mappers/MapperProfile.cs
@@ -15,10 +15,11 @@ namespace API.Mappers
                     => o.MapFrom(s => s.Attendees
                         .FirstOrDefault(x => x.IsHost).AppUser.UserName));
 
-            CreateMap<ActivityUser, Application.Profiles.Profile>()
+            CreateMap<ActivityUser, AttendeeDto>()
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
                 .ForMember(d => d.UserName, o => o.MapFrom(s => s.AppUser.UserName))
-                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio));
+                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
 
             CreateMap<AppUser, Application.Profiles.Profile>()
                 .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
